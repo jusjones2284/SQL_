b8f764b5faede06bb1df7571f467db0799e414ef
Justin Jones <jus.jones@yahoo.com>
2023-06-23 14:23:20 -0400

    working with sql

--- sql/trainning.sql
+++ sql/trainning.sql
@@ -862,3 +862,48 @@ ALTER TABLE houses2 RENAME COLUMN purchases_price to purchase_price;
 DESC houses2;
 
 
+//creating foreign keys 
+
+CREATE TABLE customers(
+id int auto_increment, 
+first_name varchar(50), 
+last_name varchar(50), 
+email varchar(50),
+primary key(id));
+
+CREATE TABLE orders(
+id int auto_increment primary key, 
+order_date date,
+amount DECIMAL(8,2), 
+customer_id INT, 
+FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+INSERT INTO customers(
+first_name, last_name, email) 
+VALUES
+('Boy', 'George', 'george@gmail.com'),
+('George', 'Michael', 'gm@gmail.com'),
+('David', 'Bowie', 'david@gmail.com'),
+('Blue', 'Steele', 'blue@gmail.com'),
+('Bette', 'Davis', 'bette@aol.com'); 
+
+INSERT INTO orders(
+order_date, amount, customer_id)
+VALUES('2016-02-10', 99.99, 1),
+	('2017-11-11', 35.50, 1),
+    ('2014-12-12', 800.67, 2),
+    ('2015-01-03', 12.50, 2),
+    ('1999-04-11', 450.25, 5);
+    
+    SELECT * FROM customers;
+
+DROP TABLE orders; 
+
+INSERT INTO orders(
+order_date, amount, customer_id) 
+VALUES('2022-11-11', 78.99, 50);
+
+
+
+
